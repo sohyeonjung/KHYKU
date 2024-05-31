8ba880dac2ecba8309c9aa094328c0349e095d1c
sohyeonjung <jhyeon23@konkuk.ac.kr>
2024-05-31 23:09:37 +0900

    UI complete

--- app/src/main/java/com/example/khyku/screen/CommentInputDialog.kt
+++ app/src/main/java/com/example/khyku/screen/CommentInputDialog.kt
@@ -1,12 +1,16 @@
 package com.example.khyku.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,9 +18,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.khyku.roomDB.Comment
 import com.example.khyku.viewmodel.CommentViewModel
 
+@ExperimentalMaterial3Api
 @Composable
 fun CommentInputDialog(
     onDismiss: () -> Unit,
@@ -37,8 +44,13 @@ fun CommentInputDialog(
             TextField(
                 value = commentText,
                 onValueChange = { commentText = it },
-                placeholder = { Text("댓글을 입력하세요.", color = Color.Black) }
+                placeholder = { Text("댓글을 입력하세요.", color = Color.Black, fontSize = 20.sp) },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.White
+                ),
+                modifier = Modifier.width(360.dp)
             )
+
         },
         confirmButton = {
             Button(onClick = {
@@ -53,6 +65,8 @@ fun CommentInputDialog(
             OutlinedButton(onClick = onDismiss) {
                 Text("취소", color = Color.Black)
             }
-        }
+        },
+        shape = RoundedCornerShape(0.dp),
+        containerColor = Color.White
     )
 }
--- app/src/main/java/com/example/khyku/screen/PostScreen.kt
+++ app/src/main/java/com/example/khyku/screen/PostScreen.kt
@@ -8,8 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -35,10 +38,14 @@ import com.example.khyku.viewmodel.CommentViewModel
 import com.example.khyku.viewmodel.CommentViewModelFactory
 
 
+@ExperimentalMaterial3Api
 @Composable
 fun PostDetailScreen(post: Post) {
 
     val KonkukGreen = Color(0xFF036B3F)
+
+    val state = rememberScrollState()
+
 
     val context = LocalContext.current
     val commentdb = CommentDatabase.getCommentDatabase(context)
@@ -60,7 +67,10 @@ fun PostDetailScreen(post: Post) {
     ) {
         Text(
             text="스터디원 구하기",
-            modifier = Modifier.fillMaxWidth().height(70.dp).padding(top=15.dp, start = 10.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(70.dp)
+                .padding(top = 15.dp, start = 10.dp),
             color = Color.White,
             fontSize = 27.sp,
             fontWeight = FontWeight.Bold
@@ -72,52 +82,67 @@ fun PostDetailScreen(post: Post) {
         ){
             Text(
                 text="사람이름",
-                modifier = Modifier.fillMaxWidth().padding(start=10.dp, top=10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp, top = 10.dp),
                 color = Color.Black,
                 fontWeight = FontWeight.Bold,
                 fontSize = 15.sp
             )
             Text(text = post.postTitle,
-                modifier = Modifier.fillMaxWidth().padding(start=10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp),
                 color = Color.Black,
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold
             )
             Text(text=post.postType,
-                modifier = Modifier.fillMaxWidth().padding(start=10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp),
                 color = Color.Black,
                 fontSize = 15.sp
             )
             Text(text = post.postContents,
-                modifier = Modifier.fillMaxWidth().padding(top=20.dp, start=10.dp, bottom=10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp, start = 10.dp, bottom = 10.dp),
                 color = Color.Black,
                 fontSize = 15.sp
             )
         }
-        Spacer(modifier = Modifier.padding(top=20.dp).background(color = KonkukGreen))
-        Column (
-            modifier = Modifier
-                .background(color = Color.LightGray)
-        ){
-            if(commentlist.isEmpty()) Text(text = "댓글 없음")
-            else CommentList(list = commentlist)
-
-            if (showCommentDialog) {
-                CommentInputDialog(
-                    onDismiss = { showCommentDialog = false }, viewModel
-                )
-            }
-        }
+        Spacer(modifier = Modifier.background(color = KonkukGreen).padding(top=20.dp))
         Box(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
+                .fillMaxWidth(),
             contentAlignment = Alignment.BottomEnd
         ) {
-            FloatingActionButton(onClick = { showCommentDialog = true },
-                modifier = Modifier.background(color = KonkukGreen)) {
+
+            Column (
+                modifier = Modifier
+                    .background(color = Color.LightGray)
+            ){
+                if(commentlist.isEmpty()) Text(text = "댓글 없음")
+                else CommentList(list = commentlist)
+
+                if (showCommentDialog) {
+                    CommentInputDialog(
+                        onDismiss = { showCommentDialog = false }, viewModel
+                    )
+                }
+            }
+             FloatingActionButton(onClick = { showCommentDialog = true },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .size(50.dp)
+                    .align(Alignment.BottomEnd),
+                containerColor = Color.LightGray)
+            {
                 Icon(Icons.Default.Add, contentDescription = "Add")
             }
+
+
         }
     }
 
